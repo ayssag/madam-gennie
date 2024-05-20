edc58d79d59d1296d7640461273d4d51350fb033
ayssag <ayssanaara@gmail.com>
2024-05-20 14:05:40 -0300

    Estiliza home

--- src/app/components/views/home/home.component.ts
+++ src/app/components/views/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    MatCardModule
+    MatButtonModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
